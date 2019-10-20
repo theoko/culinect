efdd12da81fd8a16fae64c74b8dbe1fbd71fb66f
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-10-20 09:48:33 -0400

    review screen

--- app/src/main/java/com/foodapp/foodapp/helpers/Constants.java
+++ app/src/main/java/com/foodapp/foodapp/helpers/Constants.java
@@ -2,4 +2,5 @@ package com.foodapp.foodapp.helpers;
 
 public class Constants {
     public static int REVIEW_ACTIVITY_RETURN_CODE = 100;
+    public static int CAMERA_PIC_REQUEST = 101;
 }
--- app/src/main/java/com/foodapp/foodapp/ui/review/ReviewActivity.java
+++ app/src/main/java/com/foodapp/foodapp/ui/review/ReviewActivity.java
@@ -1,16 +1,49 @@
 package com.foodapp.foodapp.ui.review;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.foodapp.foodapp.R;
 
+import static com.foodapp.foodapp.helpers.Constants.CAMERA_PIC_REQUEST;
+
 public class ReviewActivity extends AppCompatActivity {
+
+    private Button uploadBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        uploadBtn = findViewById(R.id.uploadBtn);
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
+    }
+
+    private void launchCamera() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_PIC_REQUEST) {
+//            Bitmap image = (Bitmap) data.getExtras().get("data");
+//            ImageView imageview = (ImageView) findViewById(R.id.ImageView01); //sets imageview as the bitmap
+//            imageview.setImageBitmap(image);
+        }
     }
 }
