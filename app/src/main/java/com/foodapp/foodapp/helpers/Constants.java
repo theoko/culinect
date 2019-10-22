f84ca5a6a8d6b843ccd74fa89a80481102f32173
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-10-22 11:03:05 -0400

    created notification for checking whether a user is at a restaurant or not

--- app/src/main/java/com/foodapp/foodapp/helpers/Constants.java
+++ app/src/main/java/com/foodapp/foodapp/helpers/Constants.java
@@ -7,9 +7,12 @@ public class Constants {
     public static String API_KEY = "AIzaSyDwH_zuuMFZt9ZHM2fVNhjW-Ux52rVVC5M";
 
     // Set to 0.1 for testing purposes
-    public static double FOOD_PLACE_CONFIDENCE_LEVEL = 0.01;
+    public static double FOOD_PLACE_CONFIDENCE_LEVEL = 0.7;
     public static double FOOD_ITEM_CONFIDENCE_LEVEL = 0.7;
 
     // Intent extras
     public static String PLACE_EXTRA = "placeName";
+
+    // Notification IDs
+    public static int CHECK_PLACE_ID = 150;
 }
--- app/src/main/java/com/foodapp/foodapp/receivers/PlaceReceiver.java
+++ app/src/main/java/com/foodapp/foodapp/receivers/PlaceReceiver.java
@@ -1,20 +1,40 @@
 package com.foodapp.foodapp.receivers;
 
+import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import com.foodapp.foodapp.helpers.Constants;
 
+import static com.foodapp.foodapp.helpers.Constants.CHECK_PLACE_ID;
+
 public class PlaceReceiver extends BroadcastReceiver {
+
+    public static final String CHANNEL_ID = "place_check_channel";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Bundle extras = intent.getExtras();
         if (extras != null) {
             String placeName = (String) extras.get(Constants.PLACE_EXTRA);
             Toast.makeText(context, "Location: " + placeName, Toast.LENGTH_LONG).show();
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            String notificationTitle = "At " + placeName + "?";
+            Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setContentTitle(notificationTitle)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                    .setAutoCancel(true)
+                    .setOnlyAlertOnce(true)
+                    .build();
+            notificationManager.notify(CHECK_PLACE_ID, notification);
         }
     }
 }
